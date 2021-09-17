312d968a98248cc6574ca21e15f5572057de4a2c
Wiris Rafael Januario Wernek <rafaeljwerneck43@gmail.com>
2021-09-16 22:21:00 -0300

    Adicionado conteudo sobre 'Herança'

---
+++ Aula_11-Heranca/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+	idade     uint8
+	altura    uint8
+}
+type estudante struct {
+	pessoa
+	curso     string
+	faculdade string
+}
+
+func main() {
+	fmt.Println("Herança")
+	p1 := pessoa{"João", "Pedro", 21, 178}
+	fmt.Println(p1)
+
+	e1 := estudante{p1, "ADS", "USP"}
+	fmt.Println(e1)
+
+}
